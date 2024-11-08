cf138ae1da5e5cb701147dfaa8374619be69756d
kireal1017 <kireal1017@gmail.com>
2024-11-08 13:47:45 +0900

    created base

---
+++ main.py
@@ -0,0 +1,209 @@
+import time
+import random
+from colorsys import hsv_to_rgb
+import board
+from digitalio import DigitalInOut, Direction
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display import st7789
+
+# Create the display
+cs_pin = DigitalInOut(board.CE0)
+dc_pin = DigitalInOut(board.D25)
+reset_pin = DigitalInOut(board.D24)
+BAUDRATE = 24000000
+
+spi = board.SPI()
+disp = st7789.ST7789(
+    spi,
+    height=240,
+    y_offset=80,
+    rotation=180,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+
+class Joystick:
+    def __init__(self):
+        self.cs_pin = DigitalInOut(board.CE0)
+        self.dc_pin = DigitalInOut(board.D25)
+        self.reset_pin = DigitalInOut(board.D24)
+        self.BAUDRATE = 24000000
+
+        self.spi = board.SPI()
+        self.disp = st7789.ST7789(
+                    self.spi,
+                    height=240,
+                    y_offset=80,
+                    rotation=180,
+                    cs=self.cs_pin,
+                    dc=self.dc_pin,
+                    rst=self.reset_pin,
+                    baudrate=self.BAUDRATE,
+                    )
+
+        # Input pins:
+        self.button_A = DigitalInOut(board.D5)
+        self.button_A.direction = Direction.INPUT
+
+        self.button_B = DigitalInOut(board.D6)
+        self.button_B.direction = Direction.INPUT
+
+        self.button_L = DigitalInOut(board.D27)
+        self.button_L.direction = Direction.INPUT
+
+        self.button_R = DigitalInOut(board.D23)
+        self.button_R.direction = Direction.INPUT
+
+        self.button_U = DigitalInOut(board.D17)
+        self.button_U.direction = Direction.INPUT
+
+        self.button_D = DigitalInOut(board.D22)
+        self.button_D.direction = Direction.INPUT
+
+        self.button_C = DigitalInOut(board.D4)
+        self.button_C.direction = Direction.INPUT
+
+        # Turn on the Backlight
+        self.backlight = DigitalInOut(board.D26)
+        self.backlight.switch_to_output()
+        self.backlight.value = True
+
+        # Create blank image for drawing.
+        # Make sure to create image with mode 'RGB' for color.
+        self.width = self.disp.width
+        self.height = self.disp.height
+        
+joystick = Joystick()
+
+my_image = Image.new("RGB", (joystick.width, joystick.height))
+my_draw = ImageDraw.Draw(my_image)
+
+import numpy as np
+class Character:
+    def __init__(self, width, height):
+        self.appearance = 'circle'
+        self.state = None
+        self.position = np.array([width/2 - 20, height/2 - 20, width/2 + 20, height/2 + 20])
+        # 총알 발사를 위한 캐릭터 중앙 점 추가
+        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
+        self.outline = "#FFFFFF"
+
+    def move(self, command = None):
+        if command['move'] == False:
+            self.state = None
+            self.outline = "#FFFFFF" #검정색상 코드!
+        
+        else:
+            self.state = 'move'
+            self.outline = "#FF0000" #빨강색상 코드!
+
+            if command['up_pressed']:
+                self.position[1] -= 5
+                self.position[3] -= 5
+
+            if command['down_pressed']:
+                self.position[1] += 5
+                self.position[3] += 5
+
+            if command['left_pressed']:
+                self.position[0] -= 5
+                self.position[2] -= 5
+                
+            if command['right_pressed']:
+                self.position[0] += 5
+                self.position[2] += 5
+            
+                
+        #center update
+        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2]) 
+
+
+
+class Bullet:
+    def __init__(self, position, command):
+        '''
+        position : 총알을 발사한 Character의 중앙 위치
+        command : 조이스틱 입력 값
+        '''
+        self.appearance = 'rectangle'
+        self.speed = 20
+        self.damage = 10
+        self.position = np.array([position[0]-3, position[1]-3, position[0]+3, position[1]+3])
+        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
+
+        self.direction = {'up' : False, 'down' : False, 'left' : False, 'right' : False}
+        self.state = None
+        self.outline = "#0000FF"
+        
+        if command['up_pressed']:
+            self.direction['up'] = True
+        if command['down_pressed']:
+            self.direction['down'] = True
+        if command['right_pressed']:
+            self.direction['right'] = True
+        if command['left_pressed']:
+            self.direction['left'] = True
+            
+        if command['up_pressed'] and command['dow_pressed']  and command['right_pressed'] and command['left_pressed']:
+            self.direction['up'] = True
+
+    def move(self):
+        if self.direction['up']:
+            self.position[1] -= self.speed
+            self.position[3] -= self.speed
+
+        if self.direction['down']:
+            self.position[1] += self.speed
+            self.position[3] += self.speed
+
+        if self.direction['left']:
+            self.position[0] -= self.speed
+            self.position[2] -= self.speed
+            
+        if self.direction['right']:
+            self.position[0] += self.speed
+            self.position[2] += self.speed
+
+
+# 잔상이 남지 않는 코드 & 대각선 이동 가능
+my_circle = Character(joystick.width, joystick.height)
+my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
+
+bullets = []
+while True:
+    command = {'move': False, 'up_pressed': False , 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
+    
+    if not joystick.button_U.value:  # up pressed
+        command['up_pressed'] = True
+        command['move'] = True
+
+    if not joystick.button_D.value:  # down pressed
+        command['down_pressed'] = True
+        command['move'] = True
+
+    if not joystick.button_L.value:  # left pressed
+        command['left_pressed'] = True
+        command['move'] = True
+
+    if not joystick.button_R.value:  # right pressed
+        command['right_pressed'] = True
+        command['move'] = True
+
+    if not joystick.button_A.value: # A pressed
+        bullet = Bullet(my_circle.center, command)
+        bullets.append(bullet)
+
+    my_circle.move(command)
+    for bullet in bullets:
+        bullet.move()
+        
+    #그리는 순서가 중요합니다. 배경을 먼저 깔고 위에 그림을 그리고 싶었는데 그림을 그려놓고 배경으로 덮는 결과로 될 수 있습니다.
+    my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
+    my_draw.ellipse(tuple(my_circle.position), outline = my_circle.outline, fill = (0, 0, 0))
+    
+    for bullet in bullets:
+        my_draw.rectangle(tuple(bullet.position), outline = bullet.outline, fill = (0, 0, 255))
+    #좌표는 동그라미의 왼쪽 위, 오른쪽 아래 점 (x1, y1, x2, y2)
+    joystick.disp.image(my_image)
