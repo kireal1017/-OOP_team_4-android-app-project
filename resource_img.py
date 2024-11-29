9335e7947651c3971440664516c605805aede3c7
kireal1017 <kireal1017@gmail.com>
2024-11-29 12:10:37 +0900

    make start environment

---
+++ resource_img.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+morning_background = Image.open('esw_raspberryPi_game_project/image_source/background/background_evening.png')
+sunset_background = Image.open('esw_raspberryPi_game_project/image_source/background/background_sunset.png')
+midnight_background = Image.open('esw_raspberryPi_game_project/image_source/background/background_midnight.png')
+
+start_logo = Image.open('esw_raspberryPi_game_project/image_source/start_logo_120x100.png')
